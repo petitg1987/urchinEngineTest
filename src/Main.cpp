c9b8f5e649dc16d28e9587ad720ac7057ffb3d8e
greg <petitg1987@gmail.com>
2020-12-22 15:48:41 +0100

    Reorganize urchin common

--- src/Main.cpp
+++ src/Main.cpp
@@ -102,11 +102,11 @@ void Main::initializeKeyboardMap() {
 }
 
 std::string Main::retrieveResourcesDirectory(char *argv[]) const {
-    return urchin::FileHandler::getDirectoryFrom(std::string(argv[0])) + "resources/";
+    return urchin::FileUtil::getDirectoryFrom(std::string(argv[0])) + "resources/";
 }
 
 std::string Main::retrieveSaveDirectory(char *argv[]) const {
-    return urchin::FileHandler::getDirectoryFrom(std::string(argv[0])) + "save/";
+    return urchin::FileUtil::getDirectoryFrom(std::string(argv[0])) + "save/";
 }
 
 GLFWwindow* Main::createWindow(int argc, char *argv[]) {
