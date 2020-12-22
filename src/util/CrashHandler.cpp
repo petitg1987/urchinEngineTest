b920cf4ed3ddb1f281b72db7acd0ed242ff8df3e
greg <petitg1987@gmail.com>
2020-12-22 20:31:23 +0100

    Review console log for windows

--- src/util/CrashHandler.cpp
+++ src/util/CrashHandler.cpp
@@ -1,15 +1,15 @@
 #include "CrashHandler.h"
 
 void CrashHandler::onLogContainFailure() {
-    std::cerr << "Application detected an issue during his execution. See logs for more details." << std::endl;
+    std::cerr << "Error detected during application execution. See logs for more details." << std::endl;
 }
 
 void CrashHandler::onException(const std::exception& e) {
-    urchin::Logger::instance()->logError("Error occurred: " + std::string(e.what()));
-    std::cerr << "Application has been stopped in an unexpected way (exception). See logs for more details." << std::endl;
+    urchin::Logger::instance()->logError("Exception caught: " + std::string(e.what()));
+    std::cerr << "Error detected during application execution. See logs for more details." << std::endl;
 }
 
 void CrashHandler::onSignalReceived(unsigned long signalId) {
-    urchin::Logger::instance()->logError("Signal occurred: " + std::to_string(signalId));
-    std::cerr << "Application has been stopped in an unexpected way (signal). See logs for more details." << std::endl;
+    urchin::Logger::instance()->logError("Signal caught: " + std::to_string(signalId));
+    std::cerr << "Error detected during application execution. See logs for more details." << std::endl;
 }
