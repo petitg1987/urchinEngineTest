3b6f60060f4da88aa5acf02b6ef17a7cb029e2fc
petitg1987 <petitg1987@gmail.com>
2021-12-12 10:53:42 +0100

    Extract load/save method from entities

--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -61,7 +61,7 @@ void Game::initialize() {
     //load map
     map = std::make_unique<Map>(gameRenderer3d, physicsWorld.get(), &context.getSoundEnvironment(), aiEnvironment.get());
     LoadMapCallback nullLoadMapCallback;
-    MapHandler().loadMapFromFile("map.uda", nullLoadMapCallback, map);
+    MapSaveService().loadMap("map.uda", nullLoadMapCallback, map);
     map->getObjectEntity("characterAnimate").getModel()->loadAnimation("move", "models/characterAnimate.urchinAnim");
     map->getObjectEntity("characterAnimate").getModel()->animate("move", true);
 
