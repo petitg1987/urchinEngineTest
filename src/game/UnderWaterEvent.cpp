453250c87886c55256aef16b6dea87ef31211463
petitg1987 <petitg1987@gmail.com>
2024-11-19 19:28:52 +0100

    Rename spatial sound

--- src/game/UnderWaterEvent.cpp
+++ src/game/UnderWaterEvent.cpp
@@ -6,7 +6,7 @@ using namespace urchin;
 UnderWaterEvent::UnderWaterEvent(SoundEnvironment& soundEnvironment) :
         soundEnvironment(soundEnvironment),
         bIsUnderWater(false) {
-    underWaterSound = soundEnvironment.getSoundBuilder().newManualEffect("sounds/underWater.ogg", PlayBehavior::PLAY_LOOP);
+    underWaterSound = soundEnvironment.getSoundBuilder().newManualTriggerEffect("sounds/underWater.ogg", PlayBehavior::PLAY_LOOP);
 }
 
 UnderWaterEvent::~UnderWaterEvent() {
